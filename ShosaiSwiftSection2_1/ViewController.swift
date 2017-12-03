996c93db1690d8e6af3ad39592594a4de4ce5dad
Atsushi Kubara <twinkly-swallows-realize-dream@docomo.ne.jp>
2017-12-04 01:14:07 +0900

    Initial Commit

---
+++ ShosaiSwiftSection2_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShosaiSwiftSection2_1
+//
+//  Created by atsushi on 2017/12/04.
+//  Copyright © 2017年 atsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
